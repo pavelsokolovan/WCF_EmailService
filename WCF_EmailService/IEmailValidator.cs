8ef07cf3491ad4e6b6c5859541aec09f71c9a366
pavelsokolovan <pavel.sokolov.an@gmail.com>
2015-05-26 17:06:20 +0300

    First commit

---
+++ WCF_EmailService/IEmailValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailService
+{
+    [ServiceContract]
+    public interface IEmailValidator
+    {        
+        [OperationContract]
+        bool ValiddateAdress(string emailAddress);
+    }
+}
